0f5e337925ae5b91f14570eba06c742fe8277ecc
Ahmed Elkhashen <107207031+ahmed-ae2103330@users.noreply.github.com>
2024-10-18 17:47:29 +0300

    feat(models): add User and relationships
    - Added models for Chat and Message with relationships
    - Set up one-to-many relationships between User and Message, and Chat and Message
    - Implemented Python dotenv configuration for DB_URL

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os 
 
-URL_DATABASE = 'postgresql://postgres:PASS@localhost:5432/MedPulseDB'
+db_url = os.getenv('DATABASE_URL')
+
+URL_DATABASE = db_url
 
 engine = create_engine(URL_DATABASE)
 
--- models.py
+++ models.py
@@ -1,17 +1,118 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+import datetime
+from enum import Enum
+import uuid
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String , DateTime, Table
+from sqlalchemy.orm import relationship
 from database import Base
 
-class Questions(Base):
-    __tablename__ = "questions"
 
-    id = Column(Integer, primary_key=True, index=True)
-    question_text = Column(String, index=True)
-  
-class Choices(Base):
-    __tablename__ = "choices"
+chat_participants = Table(
+    'chat_participants', Base.metadata,
+    Column('chat_id', String, ForeignKey('chats.chat_id', ondelete="CASCADE"), primary_key=True),
+    Column('user_id', String, ForeignKey('users.user_id', ondelete="CASCADE"), primary_key=True)
+)
 
-    id = Column(Integer, primary_key=True, index=True)
-    choice_text = Column(String, index=True)
-    is_correct = Column(Boolean, default=False)
-    question_id = Column(Integer, ForeignKey("questions.id"))
+class User(Base):
+    __tablename__ = 'users'
 
+    user_id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
+    settings_id = Column(String, ForeignKey('settings.id'), nullable=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    phone_no = Column(String, nullable=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    date_of_birth = Column(Date, nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    last_login = Column(DateTime, nullable=True)
+
+    logs = relationship("Log", back_populates="user", cascade="all, delete-orphan")
+    
+    settings = relationship("Settings", back_populates="user", uselist=False)
+
+    chats = relationship("Chat", secondary=chat_participants, back_populates="participants")
+
+    gender = Column(String, ForeignKey('gender.id'), nullable=False)
+    gender = relationship("Gender", back_populates="users")
+
+    messages = relationship("Message", back_populates="sender", cascade="all, delete-orphan")
+
+class Gender(Base):
+    __tablename__ = 'gender'
+
+    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
+    gender = Column(String, unique=True, nullable=False)
+
+    users = relationship("User", back_populates="gender")
+
+class AuthTypeEnum(enum.Enum):
+    email = "email"
+    sms = "sms"
+    none = "none"
+
+class Settings(Base):
+    __tablename__ = 'settings'
+
+    settings_id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
+    font_size = Column(Integer, nullable=True)
+    screen_reader = Column(Boolean, default=False)
+    on_screen_keyboard = Column(Boolean, default=False)
+    email_notifications = Column(Boolean, default=True)
+    sms_notifications = Column(Boolean, default=False)
+    email_reminders = Column(Boolean, default=False)
+    sms_reminders = Column(Boolean, default=False)
+
+    auth_type = Column(Enum(AuthTypeEnum), nullable=False)
+
+
+    user_id = Column(String, ForeignKey('users.user_id', ondelete="CASCADE"), unique=True, nullable=False)
+    user = relationship("User", back_populates="settings")
+
+
+class Log(Base):
+    __tablename__ = 'logs'
+
+    log_id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
+    action_id = Column(String, ForeignKey('actions.action_id', ondelete="CASCADE"), nullable=False)
+
+    user_id = Column(String, ForeignKey('users.user_id', ondelete="CASCADE"), nullable=False)
+    user = relationship("User", back_populates="logs")
+
+class Action(Base):
+    __tablename__ = 'actions'
+
+    action_id = Column(String, primary_key=True)
+    description = Column(String, nullable=False)
+
+    logs = relationship("Log", back_populates="action")
+
+
+class Chat(Base):
+    __tablename__ = 'chats'
+
+    chat_id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
+    name = Column(String, nullable=False)
+    creation_date = Column(DateTime, default=datetime.utcnow)
+
+    messages = relationship("Message", back_populates="chat", cascade="all, delete-orphan")
+    participants = relationship("User", secondary=chat_participants, back_populates="chats")
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    message_id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    is_read = Column(Boolean, default=False)
+    message_content = Column(String, nullable=False)
+
+    sender_id = Column(String, ForeignKey('users.user_id', ondelete="CASCADE"), nullable=False)
+
+    chat_id = Column(String, ForeignKey('chats.chat_id', ondelete="CASCADE"), nullable=False)
+
+    sender = relationship("User", back_populates="messages")
+    chat = relationship("Chat", back_populates="messages")
